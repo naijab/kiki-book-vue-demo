dc1848ddb4b63c6daac0c3336a692be6f674580c
Nattapon <nattapon@TH-LW60325-M.local>
2020-06-05 13:47:06 +0700

    Add add to cart vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,45 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { Book } from '@/type/book'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
+
+export const ADD_BOOK_TO_CART = 'ADD_TO_CART'
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    cart: [],
+  },
+  mutations: {
+    [ADD_BOOK_TO_CART](state, book: Book) {
+      const record = state.cart.find((book) => book.id === book.id)
+      if (!record) {
+        state.cart.push({
+          ...book,
+          quantity: 1,
+        })
+      } else {
+        record.quantity++
+      }
+    },
+  },
+  actions: {
+    addBookToCart({ commit }, book: Book) {
+      commit(ADD_BOOK_TO_CART, book)
+    },
+  },
+  getters: {
+    cart(state): Array<Book> {
+      return state.cart
+    },
+    cartTotal(state): number {
+      return state.cart.length > 0
+        ? Object.values(state.cart).reduce((book, currentBook) => {
+            const totalPrice = currentBook.price * currentBook.quantity
+            return book + totalPrice
+          }, 0)
+        : 0
+    },
+  },
+  modules: {},
+})
