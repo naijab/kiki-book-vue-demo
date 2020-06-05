d7e99f11a9f0d2bbb9f5a0a72388ffa29a587212
Nattapon <nattapon@TH-LW60325-M.local>
2020-06-05 09:46:39 +0700

    Add home, checkout page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+    <header class="border-b md:flex md:items-center md:justify-between p-4 pb-0 shadow-lg md:pb-4">
+      <div class="flex items-center justify-between mb-4 md:mb-0">
+        <h1 class="leading-none text-2xl text-grey-darkest">
+          <router-link to="/" class="no-underline text-grey-darkest hover:text-black">Kiki Book</router-link>
+        </h1>
+
+        <a class="text-black hover:text-orange md:hidden" href="#">
+          <i class="fa fa-2x fa-bars"></i>
+        </a>
+      </div>
+
+      <nav>
+        <ul class="list-reset md:flex md:items-center">
+          <li class="md:ml-4">
+            <router-link
+              to="/"
+              class="block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0"
+              href="#"
+            >Home</router-link>
+          </li>
+          <li class="md:ml-4">
+            <router-link
+              to="/checkout"
+              class="border-t block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0"
+              href="#"
+            >Checkout</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
     <router-view />
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="flex mb-4">
+      <div class="w-3/4 bg-gray-500 h-12"></div>
+      <div class="w-1/4 bg-gray-400 h-12"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
