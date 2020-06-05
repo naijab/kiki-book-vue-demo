e4a8835842081aedec44f14503075ec4867e9281
Nattapon <nattapon@TH-LW60325-M.local>
2020-06-05 14:49:59 +0700

    Add discount

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,43 @@ import { Book } from '@/type/book'
 
 Vue.use(Vuex)
 
-export const ADD_BOOK_TO_CART = 'ADD_TO_CART'
+const ADD_BOOK_TO_CART = 'ADD_TO_CART'
+
+const discountFromCart = (cart) => {
+  let discount = 0
+  switch (cart.length) {
+    case 2:
+      discount = 10
+      break
+    case 3:
+      discount = 20
+      break
+    case 4:
+      discount = 30
+      break
+    case 5:
+      discount = 40
+      break
+    case 6:
+      discount = 50
+      break
+    case 7:
+      discount = 60
+      break
+    default:
+      break
+  }
+  return discount
+}
+
+const sumOfCart = (cart): number => {
+  return cart.length > 0
+    ? Object.values(cart).reduce((book, currentBook) => {
+        const totalPrice = currentBook.price * currentBook.quantity
+        return book + totalPrice
+      }, 0)
+    : 0
+}
 
 export default new Vuex.Store({
   state: {
@@ -12,7 +48,9 @@ export default new Vuex.Store({
   },
   mutations: {
     [ADD_BOOK_TO_CART](state, book: Book) {
-      const record = state.cart.find((book) => book.id === book.id)
+      const record = state.cart.find(
+        (currentBook) => currentBook.id === book.id
+      )
       if (!record) {
         state.cart.push({
           ...book,
@@ -30,15 +68,23 @@ export default new Vuex.Store({
   },
   getters: {
     cart(state): Array<Book> {
+      console.log('Cart : ', state.cart)
       return state.cart
     },
     cartTotal(state): number {
-      return state.cart.length > 0
-        ? Object.values(state.cart).reduce((book, currentBook) => {
-            const totalPrice = currentBook.price * currentBook.quantity
-            return book + totalPrice
-          }, 0)
-        : 0
+      const totalSumCart = sumOfCart(state.cart)
+      const discountPercent = discountFromCart(state.cart)
+      const discount = (totalSumCart * discountPercent) / 100
+      const totalDiscount = totalSumCart - discount
+
+      //TODO: Fix Discount is Not Correct
+
+      console.log('Total Item Cart  : ', state.cart.length)
+      console.log('Total Sum Cart   : ', totalSumCart)
+      console.log('Discount Percent : ', discountPercent)
+      console.log('Discount         :', discount)
+      console.log('Total Discount   : ', totalDiscount)
+      return totalDiscount
     },
   },
   modules: {},
